6575c9041d4bbb193681387560591e88678955dd
Sebastian Porto <sebasporto@gmail.com>
2016-04-25 11:34:31 +1000

    Complete trigger action from update

--- trigger-action-from-update/Main.elm
+++ trigger-action-from-update/Main.elm
@@ -1,53 +1,90 @@
-module Main where
+module Main (..) where
 
 import StartApp
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Task exposing (Task)
 import Effects exposing (Effects, Never)
+import Signal exposing (Address)
+
 
 -- ACTIONS
 
+
 type Action
   = Click
-  | Greet
+  | Message String
+
+
 
 -- MODEL
 
-type alias Model = String
+
+type alias Model =
+  String
+
+
 
 -- VIEW
+
+
+view : Address Action -> Model -> Html
 view address model =
-    div [] [
-      div [] [
-        text model
-      ],
-      button [ onClick address Click ] [ text "Click" ]
+  div
+    []
+    [ div
+        []
+        [ text model
+        ]
+    , button [ onClick address Click ] [ text "Click" ]
     ]
+
+
 
 -- UPDATE
 
+
+{-|
+To trigger an action from update use Task.succeed
+-}
+update : Action -> Model -> ( Model, Effects Action )
 update action model =
   case action of
     Click ->
-      (model, Effects.none)
-    Greet ->
-      (model, Effects.none)
+      let
+        fx =
+          Task.succeed (Message "Hello")
+            |> Effects.task
+      in
+        ( model, fx )
+
+    Message msg ->
+      ( msg, Effects.none )
+
+
 
 -- APP
 
+
+init : ( Model, Effects Action )
 init =
-  ("Click", Effects.none)
+  ( "Click", Effects.none )
 
-app = StartApp.start {
-  view = view,
-  update = update,
-  init =init, 
-  inputs = [] 
- }
 
+app : StartApp.App Model
+app =
+  StartApp.start
+    { view = view
+    , update = update
+    , init = init
+    , inputs = []
+    }
+
+
+main : Signal Html
 main =
   app.html
+
 
 port runner : Signal (Task.Task Never ())
 port runner =
---
+++ trigger-action-from-update/readme.md
@@ -0,0 +1,1 @@
+I want to trigger an action from my update function. This pattern use `Task.succeed` for this.
