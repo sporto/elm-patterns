0a88c4f7f4b19c22467973c450613ee01ab33881
Sebastian Porto <sebasporto@gmail.com>
2016-05-23 17:44:24 +1000

    Update msg from update

--- trigger-action-from-update/readme.md
+++
@@ -1,5 +0,0 @@
-I want to trigger an action from my update function. This pattern use `Task.succeed` for this.
-
-## Running
-
-Use `elm reactor`
---
+++ trigger-message-from-update/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App
+import Html.Events exposing (onClick)
+import Task exposing (Task)
+
+-- MESSAGES
+
+type Msg
+  = Click
+  | Message String
+
+-- MODEL
+
+type alias Model =
+  String
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div
+    []
+    [ div
+        []
+        [ text model
+        ]
+    , button [ onClick Click ] [ text "Click" ]
+    ]
+
+-- UPDATE
+
+{-|
+To trigger an action from update use Task.succeed
+-}
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Click ->
+      let
+        fx =
+          Task.succeed "Hello"
+            |> Task.perform Message Message
+      in
+        ( model, fx )
+
+    Message msg ->
+      ( msg, Cmd.none )
+
+-- APP
+
+init : ( Model, Cmd Msg )
+init =
+  ( "Click", Cmd.none )
+  
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+main =
+  Html.App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
